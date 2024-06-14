e8fdb367e9ed9e40e05e9b586fdb1ab02fd00bfc
Finley <finley.mwachia12@gmail.com>
2024-06-14 09:09:02 +0300

    add models to the model.py file in cars directory

--- carModels/cars/models.py
+++ carModels/cars/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class carBrand(models.Model):
+    name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
+    
+class carModel(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.ForeignKey(carBrand,on_delete=models.CASCADE)
+    year = models.IntegerField()
+    horsepower = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.brand.name} {self.name}"
+    
